948228697112f67f47b0d2cc057f460278acb892
Alexander.Taranov <taranov.ad@phystech.edu>
2023-09-11 11:55:33 +0300

    Found bug in test, fixed bugs in ideal cache.

--- include/ideal.hpp
+++ include/ideal.hpp
@@ -10,38 +10,49 @@
 
 namespace caches
 {
-        template <typename T, typename KeyType = int>
+        template <typename T, typename key_type = int>
         struct ideal
         {
                 size_t cache_capacity_ = 0;
 
-                std::list<std::pair<KeyType, T>> cache_ {};
+                std::list<std::pair<key_type, T>> cache_ {};
                 std::list<size_t> data_occurrences_ {}; 
 
-                using            list_iterator = typename std::list<std::pair<KeyType, T>>::iterator;
-                using       data_list_iterator = typename std::list<size_t>::iterator;
-                using data_occurrence_iterator = typename std::unordered_map<KeyType, data_list_iterator>::iterator;
+                using                data_list = typename std::list<size_t>;
+                using            list_iterator = typename std::list<std::pair<key_type, T>>::iterator;
+                using data_occurrence_iterator = typename std::unordered_map<key_type, data_list>::iterator;
 
-                std::unordered_map<KeyType, list_iterator>                      hashtable_ {};
-                std::unordered_map<KeyType, data_list_iterator> data_occurrence_hashtable_ {};
+                std::unordered_map<key_type, list_iterator>             hashtable_ {};
+                std::unordered_map<key_type, data_list> data_occurrence_hashtable_ {};
 
                 ideal (size_t size) : cache_capacity_(size) {};
                 ~ideal() {};
 
-                bool cache_is_full () {
+                bool cache_is_full ()
+                {
                         return cache_.size() == cache_capacity_;
                 }
 
                 template <typename func>
-                void parse_data (T *data, size_t n_data_elements, func hash_data) {
+                void parse_data (T *data, size_t n_data_elements, func hash_data)
+                {
+                        data_occurrence_iterator occurrence_iter {};  
+                        
                         for (size_t i = 0; i < n_data_elements; i++) {
-                                data_occurrences_.emplace_front(i);
-                                data_occurrence_hashtable_.emplace(hash_data(data[i]), data_occurrences_.begin());
+                                occurrence_iter = data_occurrence_hashtable_.find(hash_data(data[i]));
+                                data_occurrence_hashtable_[hash_data(data[i])].emplace_back(i);
                         }
                 }
 
+                void check_delete_null_occurrence (key_type key)
+                {
+                        if (data_occurrence_hashtable_[key].begin() == data_occurrence_hashtable_[key].end())
+                                data_occurrence_hashtable_.erase(key);
+                }
+
                 template <typename func>
-                bool lookup_update (KeyType key, func get_data) {
+                bool lookup_update (key_type key, func get_data)
+                {
                         auto hit = hashtable_.find(key);
                         if (hit == hashtable_.end()) {
                                 if (cache_is_full()) {
@@ -50,8 +61,8 @@ namespace caches
                                 cache_.emplace_front(key, get_data(key));
                                 hashtable_.emplace(key, cache_.begin());
 
-                                data_occurrences_.erase(data_occurrence_hashtable_.find(key)->second);
-                                data_occurrence_hashtable_.erase(key);
+                                data_occurrence_hashtable_[key].pop_front();
+                                check_delete_null_occurrence(key);
                                 
                                 return false;
                         }
@@ -59,32 +70,41 @@ namespace caches
                         auto elem_iterator = hit->second;
                         if (elem_iterator != cache_.begin())
                                 cache_.splice(cache_.begin(), cache_, elem_iterator, std::next(elem_iterator));
+
+                        data_occurrence_hashtable_[key].pop_front();
+                        check_delete_null_occurrence(key);
                         
                         return true;
                 }
 
-                void delete_farthest_elem () {
+                void delete_farthest_elem ()
+                {
                         size_t greatest_index = 0;
-
-                        data_occurrence_iterator elem_occurrences {};
-                        list_iterator cached_elem_iterator {};
+        
+                        list_iterator     cached_elem_iterator   {};
+                        data_occurrence_iterator elem_occurrence {};
 
                         for (auto it = cache_.begin(); it != cache_.end(); it++) {
-                                elem_occurrences = data_occurrence_hashtable_.find(it->first);
-                                if (elem_occurrences == data_occurrence_hashtable_.end())
-                                        continue;
-                                if (*(elem_occurrences->second) > greatest_index) {
-                                        greatest_index = *(elem_occurrences->second);
+                                elem_occurrence = data_occurrence_hashtable_.find(it->first);
+                                if (elem_occurrence == data_occurrence_hashtable_.end()) {
+                                        hashtable_.erase(it->first);
+                                        cache_.erase(it);
+                                        return;
+                                }
+                                auto index = *elem_occurrence->second.begin();
+                                if (index > greatest_index) {
+                                        greatest_index = index;
                                         cached_elem_iterator = it;
                                 }
                         }
 
                         if (greatest_index) {
+                                hashtable_.erase(cached_elem_iterator->first);
                                 cache_.erase(cached_elem_iterator);
-                                data_occurrences_.erase(elem_occurrences->second);
-                                data_occurrence_hashtable_.erase(elem_occurrences);
-                        } else
+                        } else {
+                                hashtable_.erase(cache_.back().first);
                                 cache_.pop_back();
+                        }
                 }
         };
 };
--- include/two_q.hpp
+++ include/two_q.hpp
@@ -10,25 +10,26 @@
 
 namespace caches
 {
-        template <typename T, typename KeyType = int> 
+        template <typename T, typename key_type = int> 
         struct two_q 
         {
                 size_t cache_capacity_        = 0;
                 size_t hot_cache_part_divider = 4;
                 size_t in_cache_part_divider  = 4;
 
-                std::list<std::pair<KeyType, T>> hot_cache_ {};
-                std::list<std::pair<KeyType, T>>  in_cache_ {};
-                std::list<          KeyType>     out_cache_ {};
+                std::list<std::pair<key_type, T>> hot_cache_ {};
+                std::list<std::pair<key_type, T>>  in_cache_ {};
+                std::list<          key_type>     out_cache_ {};
 
-                using data_list_iterator = typename std::list<std::pair<KeyType, T>>::iterator;
-                using  out_list_iterator = typename std::list<KeyType>::iterator;
+                using data_list_iterator = typename std::list<std::pair<key_type, T>>::iterator;
+                using  out_list_iterator = typename std::list<key_type>::iterator;
 
-                std::unordered_map<KeyType, data_list_iterator> hot_hashtable_;
-                std::unordered_map<KeyType, data_list_iterator>  in_hashtable_;
-                std::unordered_map<KeyType, out_list_iterator>  out_hashtable_;
+                std::unordered_map<key_type, data_list_iterator> hot_hashtable_;
+                std::unordered_map<key_type, data_list_iterator>  in_hashtable_;
+                std::unordered_map<key_type, out_list_iterator>  out_hashtable_;
 
-                two_q (size_t size) : cache_capacity_(size) {                        
+                two_q (size_t size) : cache_capacity_(size)
+                {                        
                         if (size < MIN_2Q_CAPACITY) {
                                 std::cerr << "Minimum cache capacity is " << MIN_2Q_CAPACITY << std::endl;
                                 exit(CACHE_CAPACITY_ERROR);
@@ -41,20 +42,24 @@ namespace caches
                 size_t in_cache_capacity_  = cache_capacity_ / in_cache_part_divider;
                 size_t out_cache_capacity_ = cache_capacity_ - in_cache_capacity_ - hot_cache_capacity_;
 
-                bool hot_cache_is_full () {
+                bool hot_cache_is_full ()
+                {
                         return (hot_cache_.size() == hot_cache_capacity_);
                 }
 
-                bool inCache_is_full () {
+                bool inCache_is_full () 
+                {
                         return (in_cache_.size() == in_cache_capacity_);  
                 }
 
-                bool outCache_is_full () {
+                bool outCache_is_full () 
+                {
                         return (out_cache_.size() == out_cache_capacity_);
                 }
 
                 template <typename func>
-                void add_data2hot_cache (int key, func get_data) {
+                void add_data2hot_cache (int key, func get_data) 
+                {
                         if (hot_cache_is_full()) {
                                 hot_hashtable_.erase(hot_cache_.back().first);
                                 hot_cache_.pop_back();
@@ -63,7 +68,8 @@ namespace caches
                         hot_hashtable_.emplace(key, hot_cache_.begin());
                 }
 
-                void add_data2out_cache (int key) {
+                void add_data2out_cache (int key) 
+                {
                         if (outCache_is_full()) {
                                 out_hashtable_.erase(out_cache_.back());
                                 out_cache_.pop_back();
@@ -73,7 +79,8 @@ namespace caches
                 }
 
                 template <typename func>
-                void add_data2in_cache (int key, func get_data) {
+                void add_data2in_cache (int key, func get_data) 
+                {
                         if (inCache_is_full()) {
                                 add_data2out_cache(in_cache_.back().first);
                                 in_hashtable_.erase(in_cache_.back().first);
@@ -84,7 +91,8 @@ namespace caches
                 }
 
                 template <typename func>
-                bool lookup_update (int key, func get_data) {
+                bool lookup_update (int key, func get_data) 
+                {
                         auto hit = hot_hashtable_.find(key);
                         if (hit != hot_hashtable_.end()) {
                                 auto elem_iterator = hit->second;
